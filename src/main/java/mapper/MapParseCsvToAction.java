71836da6d5af7731a88ea3687cbe075e4acc95f3
Yura Fesun <yura.fesun@gmail.com>
2022-10-22 16:45:27 +0300

    Refactor: fixed enum and LocalDateTime

--- src/main/java/mapper/MapParseCsvToAction.java
+++ src/main/java/mapper/MapParseCsvToAction.java
@@ -7,14 +7,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class MapParseCsvToAction {
-
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+
     public Action map(String[] arr) {
         Action action = new Action();
 
-        action.setActionType(ActionType.valueOf(arr[0]));
-        action.setDateTime(LocalDateTime.parse(arr[4],FORMATTER));
-        action.setEventType(EventType.valueOf(arr[1]));
+        action.setActionType(ActionType.valueOf(arr[0].toUpperCase()));
+        action.setDateTime(LocalDateTime.parse(arr[4], FORMATTER));
+        action.setEventType(EventType.valueOf(arr[1].toUpperCase()));
         action.setEventName(arr[2]);
         action.setTicketsQuantity(Integer.parseInt(arr[3]));
 
